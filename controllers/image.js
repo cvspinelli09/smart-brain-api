739cb4896def987f6c52a0746ca07b654333424e
cvspinelli <cvspinelli09@gmail.com>
2020-04-07 20:49:35 +0100

    uploading server side to github

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "6e979390564645838a12b5abe39ad500",
+});
+
+const handleAPICall = (db) => (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
+
+const handleImage = (db) => (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("error getting entries"));
+};
+
+module.exports = {
+    handleImage,
+    handleAPICall
+}
