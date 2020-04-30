e116dddd880171d32e940da17da8ad84fbff53f2
cvspinelli <cvspinelli09@gmail.com>
2020-04-30 19:35:38 +0100

    adding port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get("/profile/:id", profile.handleProfileGet(db));
 app.put("/image", image.handleImage(db));
 app.post("/imageurl", image.handleAPICall(db));
 
-app.listen(3000, () => {
-    console.log("App listening on port 3000!");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App listening on port ${process.env.PORT}`);
 });
